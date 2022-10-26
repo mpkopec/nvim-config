d30e8a29612d49496ae55c74078272cc116518a5
Maciej Kopeć <maciejpkopec@gmail.com>
2022-10-26 10:14:57 +0200

    Correct bash installation script for vim-plug

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 if ! which curl; then
     sudo apt install curl
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+fi
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
